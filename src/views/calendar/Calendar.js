5a28a3561cc16fec42379f091fb86467eb72487d
relbv5 <relbv5@mail.umsl.edu>
2021-02-22 19:16:51 -0600

    added calendar

--- src/views/calendar/Calendar.js
+++ src/views/calendar/Calendar.js
@@ -3,8 +3,32 @@ import PropTypes from 'prop-types';
 import { CWidgetBrand, CRow, CCol } from '@coreui/react';
 import CIcon from '@coreui/icons-react';
 import ChartLineSimple from '../charts/ChartLineSimple';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction";
 
-const Calendar = ({withCharts})=>{
+export default class Calendar extends React.Component {
+  render() {
+    return (
+      <FullCalendar
+        plugins={[ dayGridPlugin, interactionPlugin ]}
+        initialView="dayGridMonth"
+        dateClick={this.handleDateClick}
+        events={[
+          { title: 'MasterCard Job', date: '2021-02-08' },
+          { title: 'UMSL Job', date: '2021-02-18' }
+        ]}
+      />
+    )
+  }
+
+  handleDateClick = (arg) => { // bind with an arrow function
+    alert(arg.dateStr)
+  }
+
+}
+
+/* const Calendar = ({withCharts})=>{
 
   // render
 
@@ -59,4 +83,4 @@ Calendar.propTypes = {
   withCharts: PropTypes.bool
 }
 
-export default Calendar
+export default Calendar */
